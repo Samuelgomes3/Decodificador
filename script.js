76dd1a1eaaa3499dac17e70f96326406fb836260
Samuel <samuelgomesdev@gmail.com>
2023-05-14 13:01:37 -0300

    Função encriptar criada

--- script.js
+++ script.js
@@ -7,4 +7,16 @@ const mensagem = document.querySelector(".mensagem");
 // A letra "o" é convertida para "ober"
 // A letra "u" é convertida para "ufat"
 
-let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+function encriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);            
+        }
+    }
+    
+    return stringEncriptada;
+}
