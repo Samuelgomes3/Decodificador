989019e3bde2b6d0a6ab47081b26207e2e78e8fa
Samuel <samuelgomesdev@gmail.com>
2023-05-14 11:09:15 -0300

    Array e seletores criados

---
+++ script.js
@@ -0,0 +1,10 @@
+const digiteTexto = document.querySelector(".digiteTexto");
+const mensagem = document.querySelector(".mensagem");
+
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
